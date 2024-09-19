fb3ad7a45857a7022e5354eccaf32428920fea74
GbrAntunes <gbr.antunes@gmail.com>
2024-09-18 22:08:27 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl text-blue-700 font-bold">
+        Hello React!
+      </h1>
+    </>
+  )
+}
+
+export default App
