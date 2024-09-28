ca1fd17d876d16919440f3181dedfdc045afebcb
Danilo <danilosoaresdasilva@hotmail.com>
2024-09-28 18:58:56 -0300

    feat: CRUD users e página admin

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react';
 import 'tailwindcss/tailwind.css';
+import { useEffect, useState } from 'react';
+import { useUsers } from '../../context/Users/UsersContext';
+import { TUserRole } from '../../types/user';
+import { deleteUser, getUserId, getUserRole, updateUser } from '../../services/user.service';
 
-type UserRole = 'teacher' | 'student';
+export function Admin() {
+  const [role, setRole] = useState<TUserRole | null>(null);
+  const { users, loading, fetchUsers } = useUsers();  
+  
+  const changeUserRole = async ({ id, newRole }: { id?: string, newRole: TUserRole }) => {
+    if (!id) return;
+    const userData = users.find(user => user._id === id);
+    if (!userData) return;
+    userData.role = newRole
+    try {
+      await updateUser({id, userData});
+    }catch (error) {
+      console.error('Erro ao alterar o perfil do usuário:', error);
+    } finally {
+      fetchUsers();
+    }
+  };
+  
+  const handleDeleteUser = async (id?: string) => {
+    if (!id) return;
+    try {
+      const user_id = getUserId();
+      await deleteUser({ id, user_id });
+    } catch (error) {
+      console.error('Erro ao deletar o usuário:', error);
+    } finally {
+      fetchUsers();
+    }
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-  role: UserRole;
-}
+  };
+  
+  useEffect(() => {
+    const userRole = getUserRole();
+    if (userRole) {
+      setRole(userRole);
+    }
+  }, [role]);
 
-const mockUsers: User[] = [
-  { id: 1, name: 'John Doe', email: 'john@example.com', role: 'student' },
-  { id: 2, name: 'Jane Smith', email: 'jane@example.com', role: 'teacher' },
-];
-
-export default function Admin() {
-  const [role, setRole] = useState<UserRole>('teacher'); // Simulação de autenticação
-  const [users, setUsers] = useState<User[]>(mockUsers);
-
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers])
+  
   if (role !== 'teacher') {
     return <h1 className="text-center text-red-500">Acesso negado. Apenas professores podem acessar esta página.</h1>;
   }
-
-  const changeUserRole = (id: number, newRole: UserRole) => {
-    setUsers(users.map(user => (user.id === id ? { ...user, role: newRole } : user)));
-  };
-
-  const deleteUser = (id: number) => {
-    setUsers(users.filter(user => user.id !== id));
-  };
+  if (loading) {
+    return <div className="min-h-screen bg-gray-100 p-4 flex items-center justify-center">Carregando...</div>;
+  }
 
   return (
     <div className="min-h-screen bg-gray-100 p-4">
@@ -46,27 +68,27 @@ export default function Admin() {
           </thead>
           <tbody>
             {users.map(user => (
-              <tr key={user.id}>
+              <tr key={user._id}>
                 <td className="py-2 px-4 border-b">{user.name}</td>
                 <td className="py-2 px-4 border-b">{user.email}</td>
                 <td className="py-2 px-4 border-b">{user.role}</td>
                 <td className="py-2 px-4 border-b">
                   <button
-                    onClick={() => changeUserRole(user.id, 'teacher')}
+                    onClick={() => changeUserRole({id: user._id, newRole:'teacher'})}
                     className={`bg-blue-500 text-white px-2 py-1 rounded mr-2 ${user.role === 'teacher' ? 'opacity-50 cursor-not-allowed' : ''}`}
                     disabled={user.role === 'teacher'}
                   >
                     Tornar Professor
                   </button>
                   <button
-                    onClick={() => changeUserRole(user.id, 'student')}
+                    onClick={() => changeUserRole({id: user._id, newRole:'student'})}
                     className={`bg-green-500 text-white px-2 py-1 rounded mr-2 ${user.role === 'student' ? 'opacity-50 cursor-not-allowed' : ''}`}
-                    disabled={user.role === 'student'}
+                    disabled={user.role !== 'teacher'}
                   >
                     Tornar Aluno
                   </button>
                   <button
-                    onClick={() => deleteUser(user.id)}
+                    onClick={() => handleDeleteUser(user._id)}
                     className="bg-red-500 text-white px-2 py-1 rounded"
                   >
                     Deletar
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,7 @@
 import { TUser, TUserRole } from "../types/user";
+import { httpRequest } from "./http.services";
+
+const BASE_API = import.meta.env.VITE_BASE_API;
 
 export const getUserRole = (): TUserRole | null => {
   const user = localStorage.getItem('user');
@@ -25,4 +28,29 @@ export const getUserId = (): string => {
     return userData._id || '';
   }
   return '';
+}
+
+export const getAllUsers = async (): Promise<TUser[]> => {
+    const response = await httpRequest(`${BASE_API}users`, { method: 'GET' });
+    return response as TUser[] || [];
+}
+
+export const getUserById = async (id: string): Promise<TUser | null> => {
+   const response = await httpRequest(`${BASE_API}users/${id}`, { method: 'GET' });
+    return response as TUser;
+}
+
+export const createUser = async (user: TUser): Promise<TUser> => {
+    const response = await httpRequest(`${BASE_API}users`, { method: 'POST', body: user });
+    return response as TUser;
+}
+
+export const updateUser = async ({ id, userData }:{id: string, userData: TUser}): Promise<TUser> => {
+    const response = await httpRequest(`${BASE_API}users/${id}`, { method: 'PUT', body: userData });
+    return response as TUser;
+}
+
+export const deleteUser = async ({ id, user_id }:{id: string, user_id: string}): Promise<boolean> => {
+  await httpRequest(`${BASE_API}users/${id}`, { method: 'DELETE', body: { user_id } });
+  return true;
 }
